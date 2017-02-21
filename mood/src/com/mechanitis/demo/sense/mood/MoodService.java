1339fe59445efb4970b20a360fdc955cda3cbb45
Trisha Gee <trisha.gee@gmail.com>
2017-02-21 13:15:14 +0100

    Adding skeleton for Mood service

---
+++ mood/src/com/mechanitis/demo/sense/mood/MoodService.java
@@ -0,0 +1,13 @@
+package com.mechanitis.demo.sense.mood;
+
+import com.mechanitis.demo.sense.service.Service;
+
+public class MoodService {
+    private static final int PORT = 8082;
+    private final Service service;
+
+    public MoodService() {
+        service = null;
+    }
+
+}
