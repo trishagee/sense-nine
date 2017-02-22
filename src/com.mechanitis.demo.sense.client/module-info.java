6df955768ff9b80b13a427b7296402170ad65d30
Trisha Gee <trisha.gee@gmail.com>
2017-02-22 15:24:44 +0100

    ClientEndpoint added into the client code, so it can connect to back end services

--- src/com.mechanitis.demo.sense.client/module-info.java
+++ src/com.mechanitis.demo.sense.client/module-info.java
@@ -1,10 +1,29 @@
 module com.mechanitis.demo.sense.client {
+    requires java.logging;
+
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.base;
 
+    /* Websocket dependencies */
+    requires javax.websocket.api;
+    requires javax.websocket.client.impl;
+
+    requires websocket.common;
+    requires websocket.client;
+    requires websocket.api;
+
+    requires jetty.util;
+    requires jetty.io;
+    requires jetty.client;
+    requires jetty.http;
+
     exports com.mechanitis.demo.sense.client;
+    exports com.mechanitis.demo.sense.client.service;
+    //not sure this is what I want
+    exports com.mechanitis.demo.sense.client.user;
+
     opens com.mechanitis.demo.sense.client to javafx.fxml;
     opens com.mechanitis.demo.sense.client.mood to javafx.fxml;
     opens com.mechanitis.demo.sense.client.user to javafx.fxml;
