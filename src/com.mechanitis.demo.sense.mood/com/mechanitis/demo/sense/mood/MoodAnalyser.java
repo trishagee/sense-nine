97b768e3e189d0c981cd1bee8cccc030070b23da
Trisha Gee <trisha.gee@gmail.com>
2017-02-23 19:18:27 +0100

    MoodAnalyser does exactly the same thing as before, but using RxJava.

--- src/com.mechanitis.demo.sense.mood/com/mechanitis/demo/sense/mood/MoodAnalyser.java
+++ src/com.mechanitis.demo.sense.mood/com/mechanitis/demo/sense/mood/MoodAnalyser.java
@@ -1,48 +1,49 @@
 package com.mechanitis.demo.sense.mood;
 
-import java.util.HashMap;
+import io.reactivex.Observable;
+
 import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.StringJoiner;
 
 import static com.mechanitis.demo.sense.mood.Mood.HAPPY;
 import static com.mechanitis.demo.sense.mood.Mood.SAD;
 import static com.mechanitis.demo.sense.twitter.TweetParser.getTweetMessageFrom;
+import static java.util.Map.entry;
+import static java.util.Map.ofEntries;
 
 public class MoodAnalyser {
-    private static final Map<String, Mood> WORD_TO_MOOD = new HashMap<>();
-
-    static {
-        WORD_TO_MOOD.put("happy", HAPPY);
-        WORD_TO_MOOD.put("good", HAPPY);
-        WORD_TO_MOOD.put("great", HAPPY);
-        WORD_TO_MOOD.put("keen", HAPPY);
-        WORD_TO_MOOD.put("awesome", HAPPY);
-        WORD_TO_MOOD.put("marvelous", HAPPY);
-        WORD_TO_MOOD.put("yay", HAPPY);
-        WORD_TO_MOOD.put("pleased", HAPPY);
-        WORD_TO_MOOD.put("sad", SAD);
-        WORD_TO_MOOD.put("mad", SAD);
-        WORD_TO_MOOD.put("blargh", SAD);
-        WORD_TO_MOOD.put("boo", SAD);
-        WORD_TO_MOOD.put("terrible", SAD);
-        WORD_TO_MOOD.put("horrible", SAD);
-        WORD_TO_MOOD.put("bad", SAD);
-        WORD_TO_MOOD.put("awful", SAD);
-    }
+    private static final Map<String, Mood> WORD_TO_MOOD =
+            ofEntries(entry("happy", HAPPY),
+                      entry("good", HAPPY),
+                      entry("great", HAPPY),
+                      entry("keen", HAPPY),
+                      entry("awesome", HAPPY),
+                      entry("marvelous", HAPPY),
+                      entry("yay", HAPPY),
+                      entry("pleased", HAPPY),
+                      entry("sad", SAD),
+                      entry("mad", SAD),
+                      entry("blargh", SAD),
+                      entry("boo", SAD),
+                      entry("terrible", SAD),
+                      entry("horrible", SAD),
+                      entry("bad", SAD),
+                      entry("awful", SAD));
 
     private MoodAnalyser() {
     }
 
     public static String analyseMood(String fullMessage) {
-        String[] wordsInMessage = getTweetMessageFrom(fullMessage).split("\\s");
-        return Stream.of(wordsInMessage)
-                     .map(String::toLowerCase)
-                     .map(WORD_TO_MOOD::get)
-                     .filter(Objects::nonNull)
-                     .distinct()
-                     .map(Enum::name)
-                     .collect(Collectors.joining(","));
+        String[] words = getTweetMessageFrom(fullMessage).split("\\s");
+        StringJoiner csvCreator = new StringJoiner(",");
+
+        Observable.fromArray(words)
+                  .map(String::toLowerCase)
+                  .filter(WORD_TO_MOOD::containsKey)
+                  .map(WORD_TO_MOOD::get)
+                  .distinct()
+                  .subscribe(mood -> csvCreator.add(mood.name()));
+
+        return csvCreator.toString();
     }
 }
--- src/com.mechanitis.demo.sense.mood/module-info.java
+++ src/com.mechanitis.demo.sense.mood/module-info.java
@@ -2,4 +2,5 @@ module com.mechanitis.demo.sense.mood {
     requires com.mechanitis.demo.sense.service;
     requires javax.websocket.api;
     requires com.mechanitis.demo.sense.twitter;
+    requires rxjava;
 }
