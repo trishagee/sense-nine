94e51608bc1277c7a4ce924d21346032f4e0a8e8
Trisha Gee <trisha.gee@gmail.com>
2017-03-05 22:37:42 +0100

    Step 4.1: Collection factory methods

--- test/com.mechanitis.demo.sense.mood/com/mechanitis/demo/sense/user/MoodTestData.java
+++ test/com.mechanitis.demo.sense.mood/com/mechanitis/demo/sense/user/MoodTestData.java
@@ -2,15 +2,16 @@ package com.mechanitis.demo.sense.user;
 
 import com.mechanitis.demo.sense.service.StubService;
 
+import java.util.List;
 import java.util.Random;
 
 public class MoodTestData {
-    private static final String[] POSSIBLE_MOODS
-            = new String[]{"HAPPY", "SAD", "HAPPY,SAD"};
+    private static final List<String> POSSIBLE_MOODS
+            = List.of("HAPPY", "SAD", "HAPPY,SAD");
 
     public static void main(String[] args) {
         Random random = new Random();
         new StubService("/moods/", 8082,
-                        () -> POSSIBLE_MOODS[random.nextInt(3)]).run();
+                () -> POSSIBLE_MOODS.get(random.nextInt(3))).run();
     }
 }
