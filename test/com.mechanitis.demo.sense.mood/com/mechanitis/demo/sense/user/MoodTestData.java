f6262f265c5df527ce167cec1e422428a17c446c
Trisha Gee <trisha.gee@gmail.com>
2017-05-18 11:40:24 +0200

    Test data uses Collections

--- test/com.mechanitis.demo.sense.mood/com/mechanitis/demo/sense/user/MoodTestData.java
+++ test/com.mechanitis.demo.sense.mood/com/mechanitis/demo/sense/user/MoodTestData.java
@@ -2,15 +2,31 @@ package com.mechanitis.demo.sense.user;
 
 import com.mechanitis.demo.sense.service.StubService;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
-public class MoodTestData {
-    private static final String[] POSSIBLE_MOODS
-            = new String[]{"HAPPY", "SAD", "HAPPY,SAD"};
+import static java.util.Arrays.asList;
+
+class MoodTestData {
+    private static final Set<String> POSSIBLE_MOODS
+            = new HashSet<>(asList("HAPPY", "SAD"));
 
     public static void main(String[] args) {
         Random random = new Random();
         new StubService("/moods/", 8082,
-                        () -> POSSIBLE_MOODS[random.nextInt(3)]).run();
+                () -> getRandomMood(random.nextInt(2))).run();
     }
+
+    private static String getRandomMood(int requiredIndex) {
+        int i = 0;
+        for (String handle : POSSIBLE_MOODS) {
+            if (i == requiredIndex) {
+                return handle;
+            }
+            i = i + 1;
+        }
+        return null;
+    }
+
 }
