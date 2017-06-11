5f522b8a7c0c1551e12bc963fb38e5b06ec960ab
Trisha Gee <trisha.gee@gmail.com>
2017-06-11 12:38:38 +0200

    Simplified module-info.java files thanks to build 173.

--- src/com.mechanitis.demo.sense.service/module-info.java
+++ src/com.mechanitis.demo.sense.service/module-info.java
@@ -1,24 +1,9 @@
 module com.mechanitis.demo.sense.service {
     requires java.logging;
-
     requires javax.websocket.api;
-    requires javax.websocket.server.impl;
-    requires javax.websocket.client.impl;
-    requires javax.servlet.api;
-
-    requires websocket.common;
-    requires websocket.client;
-    requires websocket.server;
-    requires websocket.servlet;
-    requires websocket.api;
-
     requires jetty.server;
     requires jetty.servlet;
-    requires jetty.util;
-    requires jetty.io;
-    requires jetty.client;
-    requires jetty.http;
-    requires jetty.security;
+    requires javax.websocket.server.impl;
 
     exports com.mechanitis.demo.sense.service;
 }
--- src/com.mechanitis.demo.sense.twitter/module-info.java
+++ src/com.mechanitis.demo.sense.twitter/module-info.java
@@ -1,8 +1,4 @@
 module com.mechanitis.demo.sense.twitter {
     requires com.mechanitis.demo.sense.service;
-    requires com.mechanitis.demo.sense.flow;
-
     requires java.logging;
-
-    exports com.mechanitis.demo.sense.twitter;
 }
