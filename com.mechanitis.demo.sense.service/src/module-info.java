d97a23c393ec536624dee9e1665d22940b9fbeda
Trisha Gee <trisha.gee@gmail.com>
2017-07-13 18:23:42 +0200

    Updated libraries so Mockito works properly

--- com.mechanitis.demo.sense.service/src/module-info.java
+++ com.mechanitis.demo.sense.service/src/module-info.java
@@ -1,6 +1,5 @@
 module com.mechanitis.demo.sense.service {
     requires java.logging;
-    requires javax.websocket.api;
     requires jetty.server;
     requires jetty.servlet;
     requires javax.websocket.server.impl;
