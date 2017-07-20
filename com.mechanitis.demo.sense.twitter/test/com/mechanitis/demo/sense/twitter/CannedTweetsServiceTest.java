9173ede22bfdeade2ca97f402858d5b5860614fa
Christian Stein <sormuras@gmail.com>
2017-07-20 02:06:02 +0200

    Unzip tweets if not already unzipped

--- com.mechanitis.demo.sense.twitter/test/com/mechanitis/demo/sense/twitter/CannedTweetsServiceTest.java
+++ com.mechanitis.demo.sense.twitter/test/com/mechanitis/demo/sense/twitter/CannedTweetsServiceTest.java
@@ -1,13 +1,20 @@
 package com.mechanitis.demo.sense.twitter;
 
 import com.mechanitis.demo.sense.service.MessageReceivedEndpoint;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import static com.mechanitis.demo.sense.service.ServiceFixture.connectAndWaitForSuccess;
 import static org.hamcrest.CoreMatchers.is;
@@ -15,6 +22,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 class CannedTweetsServiceTest {
     private final ExecutorService executor = Executors.newFixedThreadPool(5);
+
+    @BeforeAll
+    static void unzipTweetData() throws Exception {
+        if (Files.notExists(Paths.get("tweetdata60-mins.txt"))) {
+            String fileZip = "tweetdata60-mins.txt.zip";
+            byte[] buffer = new byte[1024];
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                String fileName = zipEntry.getName();
+                File newFile = new File(fileName);
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                zipEntry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            zis.close();
+        }
+    }
 
     @Test
     void shouldMessageClientsWithTweetsReceived() throws Exception {
@@ -26,7 +56,8 @@ class CannedTweetsServiceTest {
         CountDownLatch latch = new CountDownLatch(1);
         MessageReceivedEndpoint clientEndpoint = new MessageReceivedEndpoint(latch);
 
-        boolean success = connectAndWaitForSuccess(URI.create("ws://localhost:8081/tweets/"), clientEndpoint, latch);
+        boolean success = connectAndWaitForSuccess(URI.create("ws://localhost:8081/tweets/"),
+                                                   clientEndpoint, latch);
         assertThat("Client endpoint should have received a message", success, is(true));
 
         // finally
