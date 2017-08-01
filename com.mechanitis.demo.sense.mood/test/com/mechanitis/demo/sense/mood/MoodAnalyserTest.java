a76d6eb022a75a5b21232d6fc745a7bd96894dc4
Trisha Gee <trisha.gee@gmail.com>
2017-08-01 14:37:58 +0200

    Changes for 2017.2 blog post

--- com.mechanitis.demo.sense.mood/test/com/mechanitis/demo/sense/mood/MoodAnalyserTest.java
+++ com.mechanitis.demo.sense.mood/test/com/mechanitis/demo/sense/mood/MoodAnalyserTest.java
@@ -1,9 +1,15 @@
 package com.mechanitis.demo.sense.mood;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 import static com.mechanitis.demo.sense.mood.MoodService.filterMessagesForMoods;
 import static java.lang.String.format;
@@ -13,6 +19,14 @@ class MoodAnalyserTest {
     private static final String TWITTER_MESSAGE_TEMPLATE = "tweet = {\"created_at\":\"Tue Jan 27 12:37:11 +0000 2015\"," +
             "\"id\":560053908144275456,\"id_str\":\"560053908144275456\"," +
             "\"text\":\"%s\",\"source\":\"twitter\"}";
+
+    static Stream<Collection> createCollection() {
+        return Stream.of(Arrays.asList());
+    }
+
+    static Stream<String[]> createArray() {
+        return null;
+    }
 
 
     @DisplayName("Should correctly identify happy messages regardless of case")
@@ -29,11 +43,11 @@ class MoodAnalyserTest {
     @Test
     @DisplayName("should correctly identify sad messages")
     void ShouldIdentifySadMessages() {
-//        when:
+        // when:
         String moodyMessage = filterMessagesForMoods(format(TWITTER_MESSAGE_TEMPLATE,
                 "I am so sad today"));
 
-//        then:
+        // then:
         assertEquals("SAD", moodyMessage);
     }
 
@@ -69,5 +83,69 @@ class MoodAnalyserTest {
 //        then:
         assertEquals("", moodyMessage);
     }
+
+    @ParameterizedTest
+    @MethodSource(names = "createCollection")
+    void shouldReportMethodsThatDoNotMakeSenseOnEmptyCollections(List<String> collection) {
+        if (!(collection.size() > 0)) {
+            return;
+        }
+
+        if (collection.size() == 5) {
+            Object value = collection.get(6);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource(names = "createCollection")
+    void shouldReportMethodsThatDoNotMakeSenseOnEmptyLists(List<String> collection) {
+        if (collection.isEmpty()) {
+            String s = collection.get(0);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource(names = "createCollection")
+    void shouldReportMethodsThatDoNotMakeSenseOnEmptyMaps(Map<Integer, String> map) {
+        if (map.isEmpty()) {
+            boolean isEmpty = map.isEmpty();
+            Object key = map.containsKey(0);
+            Object value = map.containsValue("Str");
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource(names = "createCollection")
+    void shouldReportMethodsThatDoNotMakeSenseOnEmptySets(Set<String> set) {
+        if (set.isEmpty()) {
+            boolean isEmpty = set.isEmpty();
+            Object value = set.contains("String");
+        }
+    }
+
+    @Test
+    void shouldGiveNullabilityWarning() {
+        ArrayList<@Nullable String> list = new ArrayList<>();
+        doSomethingWithAListOfNonNullElements(list);
+    }
+
+    private void doSomethingWithAListOfNonNullElements(List<@NotNull String> list) {
+        //do something
+        for (String s : list) {
+            //blah
+            //blah
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource(names = "createArray")
+    void reportIfIndexOutOfRange(String[] strings) {
+        if (strings.length <= 5) {
+            System.out.println(strings[6]);
+        }
+    }
+
+
+
 
 }
