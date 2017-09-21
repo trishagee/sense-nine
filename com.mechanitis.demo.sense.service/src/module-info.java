0c906e18262d83d15b8bf4a56e711b9de8c3856b
Trisha Gee <trisha.gee@gmail.com>
2017-09-21 11:17:06 +0000

    Non-module project

--- com.mechanitis.demo.sense.service/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mechanitis.demo.sense.service {
-    requires java.logging;
-    requires javax.websocket.api;
-    requires jetty.server;
-    requires jetty.servlet;
-    requires javax.websocket.server.impl;
-
-    exports com.mechanitis.demo.sense.service;
-}
