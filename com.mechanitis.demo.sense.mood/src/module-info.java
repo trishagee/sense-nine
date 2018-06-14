1856050e092938d642bbf5ff499367e1e1fdfc0f
Trisha Gee <trisha.gee@gmail.com>
2018-06-14 17:48:18 +0200

    Generating new module info files for updated modules

---
+++ com.mechanitis.demo.sense.mood/src/module-info.java
@@ -0,0 +1,6 @@
+module com.mechanitis.demo.sense.mood {
+    requires reactive.streams;
+    requires reactor.core;
+    requires com.mechanitis.demo.sense.flow;
+    requires com.mechanitis.demo.sense.service;
+}
