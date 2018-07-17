c5c067b265e0265c8c54c27b1ebc2fb1b585e9df
Trisha Gee <trisha.gee@gmail.com>
2018-07-17 17:50:39 +0200

    Opening to reflection

--- com.mechanitis.demo.sense.client/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.client/src/main/java/module-info.java
@@ -7,7 +7,5 @@ module com.mechanitis.demo.sense.client {
 
     exports com.mechanitis.demo.sense.client to javafx.graphics;
 
-    opens com.mechanitis.demo.sense.client to javafx.fxml;
-    opens com.mechanitis.demo.sense.client.mood to javafx.fxml;
-    opens com.mechanitis.demo.sense.client.user to javafx.fxml, javafx.base;
+    opens com.mechanitis.demo.sense.client;
 }
