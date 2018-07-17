8f05cd3ffb7375aac47df60bb7f7cb629b6299d5
Trisha Gee <trisha.gee@gmail.com>
2018-07-17 18:01:20 +0200

    Restricting reflective access

--- com.mechanitis.demo.sense.client/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.client/src/main/java/module-info.java
@@ -7,5 +7,7 @@ module com.mechanitis.demo.sense.client {
 
     exports com.mechanitis.demo.sense.client to javafx.graphics;
 
-    opens com.mechanitis.demo.sense.client;
+    opens com.mechanitis.demo.sense.client to javafx.fxml;
+    opens com.mechanitis.demo.sense.client.mood to javafx.fxml;
+    opens com.mechanitis.demo.sense.client.user to javafx.fxml, javafx.base;
 }
