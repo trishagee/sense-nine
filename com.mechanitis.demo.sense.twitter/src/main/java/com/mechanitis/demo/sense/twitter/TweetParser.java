139990825eb3f9f58bdcdf89db7e0bf4b95dc7ab
Trisha Gee <trisha.gee@gmail.com>
2018-07-19 12:01:22 +0200

    Code duplication

--- com.mechanitis.demo.sense.twitter/src/main/java/com/mechanitis/demo/sense/twitter/TweetParser.java
+++ com.mechanitis.demo.sense.twitter/src/main/java/com/mechanitis/demo/sense/twitter/TweetParser.java
@@ -3,19 +3,17 @@ package com.mechanitis.demo.sense.twitter;
 public interface TweetParser {
 
     static String getTweetMessageFrom(String fullTweet) {
-        //very crude
         String textFieldName = "\"text\":\"";
-        return getValueForField(fullTweet, textFieldName);
+        int indexOfTwitterHandleField = fullTweet.indexOf(textFieldName)+ textFieldName.length();
+        int indexOfEndOfTwitterHandle = fullTweet.indexOf("\"", indexOfTwitterHandleField);
+        return fullTweet.substring(indexOfTwitterHandleField, indexOfEndOfTwitterHandle);
     }
 
     static String getTwitterHandleFromTweet(String fullTweet) {
         String twitterHandleFieldName = "\"screen_name\":\"";
-        return getValueForField(fullTweet, twitterHandleFieldName);
-    }
-
-    private static String getValueForField(String fullTweet, String fieldName) {
-        int indexOfTwitterHandleField = fullTweet.indexOf(fieldName)+ fieldName.length();
+        int indexOfTwitterHandleField = fullTweet.indexOf(twitterHandleFieldName)+ twitterHandleFieldName.length();
         int indexOfEndOfTwitterHandle = fullTweet.indexOf("\"", indexOfTwitterHandleField);
         return fullTweet.substring(indexOfTwitterHandleField, indexOfEndOfTwitterHandle);
     }
+
 }
