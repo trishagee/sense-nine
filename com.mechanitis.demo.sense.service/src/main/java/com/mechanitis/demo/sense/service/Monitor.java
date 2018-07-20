332ebe867a486f4b4e17a89b9409222e4cea6c36
Trisha Gee <trisha.gee@gmail.com>
2018-07-20 09:56:00 +0200

    Adding more flexible method for fetching process info

--- com.mechanitis.demo.sense.service/src/main/java/com/mechanitis/demo/sense/service/Monitor.java
+++ com.mechanitis.demo.sense.service/src/main/java/com/mechanitis/demo/sense/service/Monitor.java
@@ -1,26 +1,50 @@
 package com.mechanitis.demo.sense.service;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
+@SuppressWarnings({"WeakerAccess", "DeprecatedIsStillUsed"})
 public class Monitor {
 
     public static void main(String[] args) {
-        Thread.currentThread().stop();
+        printProcessInformation();
+    }
 
-        ProcessHandle.allProcesses()
-                     .map(ProcessHandle::info)
-                     .filter(processInfo -> processInfo.user()
-                                                       .filter(user -> user.equals("trishagee"))
-                                                       .isPresent())
-                     .filter(processInfo -> processInfo.command()
-                                                       .filter(command -> command.contains("java"))
-                                                       .isPresent())
-                     .filter(processInfo -> processInfo.arguments().isPresent())
-                     .flatMap(processInfo -> processInfo.arguments().stream()) // new in Java 9
-                     .flatMap(Arrays::stream)
-                     .filter(s -> s.matches("^com\\.mechanitis.*"))
-                     .forEach(processInfo -> System.out.println("processInfo = " + processInfo));
+    /**
+     * @deprecated this will be removed,
+     * use {@link Monitor#fetchProcessInfo(String, String, String)} ()} instead
+     */
+    @Deprecated
+    static void printProcessInformation() {
+        ProcessHandle
+        .allProcesses()
+        .map(ProcessHandle::info)
+        .filter(proc -> proc.user()
+                            .filter(user -> user.equals("trishagee"))
+                            .isPresent())
+        .filter(proc -> proc.command()
+                            .filter(command -> command.contains("java"))
+                            .isPresent())
+        .filter(proc -> proc.arguments().isPresent())
+        .flatMap(proc -> proc.arguments().stream())
+        .flatMap(Arrays::stream)
+        .filter(s -> s.matches("^com\\.mechanitis.*"))
+        .forEach(processInfo -> System.out.println("processInfo = " + processInfo));
+    }
 
-        final long pid = ProcessHandle.current().pid();
+    private static Stream<String> fetchProcessInfo(final String packageRegex, final String userName, final String processType) {
+        return ProcessHandle
+        .allProcesses()
+        .map(ProcessHandle::info)
+        .filter(proc -> proc.user()
+                            .filter(user -> user.equals(userName))
+                            .isPresent())
+        .filter(proc -> proc.command()
+                            .filter(command -> command.contains(processType))
+                            .isPresent())
+        .filter(proc -> proc.arguments().isPresent())
+        .flatMap(proc -> proc.arguments().stream())
+        .flatMap(Arrays::stream)
+        .filter(s -> s.matches(packageRegex));
     }
 }
