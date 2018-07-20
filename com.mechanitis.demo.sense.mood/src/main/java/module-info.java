8fd6490c813d9096c5aea2b6c25811591624f7e5
Trisha Gee <trisha.gee@gmail.com>
2018-07-20 12:45:02 +0200

    Updating all libraries, except websockets which doesn't want to play nicely.

--- com.mechanitis.demo.sense.mood/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.mood/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module com.mechanitis.demo.sense.mood {
     requires com.mechanitis.demo.sense.service;
     requires com.mechanitis.demo.sense.flow;
     requires reactor.core;
-    requires reactive.streams;
+    requires org.reactivestreams;
 
 }
--- com.mechanitis.demo.sense.service/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.service/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.mechanitis.demo.sense.service {
     requires java.logging;
     requires jetty.server;
     requires jetty.servlet;
-    requires javax.websocket.server.impl;
     requires javax.websocket.api;
 
     exports com.mechanitis.demo.sense.service;
