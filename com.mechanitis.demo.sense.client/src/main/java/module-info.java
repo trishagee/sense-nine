755a7e3299bfc472aab8c79c2508ed951349d471
Trisha Gee <trisha.gee@gmail.com>
2019-04-02 12:40:50 +0200

    JavaFX 11. Compiles in the UI (not sure the command line runs) but does not run

--- com.mechanitis.demo.sense.client/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.client/src/main/java/module-info.java
@@ -1,13 +1,10 @@
 module com.mechanitis.demo.sense.client {
-    requires com.mechanitis.demo.sense.service;
-
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires com.mechanitis.demo.sense.service;
 
-    exports com.mechanitis.demo.sense.client to javafx.graphics;
+    exports com.mechanitis.demo.sense.client;
 
-    opens com.mechanitis.demo.sense.client to javafx.fxml;
-    opens com.mechanitis.demo.sense.client.mood to javafx.fxml;
-    opens com.mechanitis.demo.sense.client.user to javafx.fxml, javafx.base;
 }
