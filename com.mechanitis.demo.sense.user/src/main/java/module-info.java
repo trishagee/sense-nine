c8337f0828384e332d45b5c94a1562e64e9b2295
Trisha Gee <trisha.gee@gmail.com>
2020-06-02 14:34:04 +0200

    Adding in User. Migrating build file to 6.4

--- com.mechanitis.demo.sense.user/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.user/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module com.mechanitis.demo.sense.user {
     requires com.mechanitis.demo.sense.service;
     requires com.mechanitis.demo.sense.flow;
 
+    // TODO: split the test application into its own module
+    requires com.mechanitis.demo.sense.service.test;
 }
