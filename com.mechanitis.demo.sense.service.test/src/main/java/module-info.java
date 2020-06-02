be912c3644aa303861374793132ccddcadd63000
Trisha Gee <trisha.gee@gmail.com>
2020-06-02 14:06:44 +0200

    Adding in Service Test. Needed changes to service

--- com.mechanitis.demo.sense.service.test/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.service.test/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module com.mechanitis.demo.sense.service.test {
-    exports com.mechanitis.demo.sense.service.test;
-
     requires com.mechanitis.demo.sense.service;
 
+    exports com.mechanitis.demo.sense.service.test;
 }
--- com.mechanitis.demo.sense.service/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.service/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.mechanitis.demo.sense.service {
     requires java.logging;
     requires jetty.server;
     requires jetty.servlet;
-    requires javax.websocket.api;
+    requires transitive javax.websocket.api;
     requires javax.websocket.server.impl;
 
     exports com.mechanitis.demo.sense.service;
