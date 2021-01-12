063dbb571b2612958ed0e2f13b66072e9a926253
Trisha Gee <trisha.gee@gmail.com>
2021-01-12 18:51:15 +0100

    Modularity for Gradle (6.8). At this stage I've removed a bunch of the modules from the application and have focused on getting the Flow and Service modules building correctly with Gradle. I've removed the shared build.gradle file and am moving all Gradle config into the individual build files. To get unnamed, non-modular libraries working, I've used a Gradle plugin which can pretty much get this working for all libraries on the classpath, which I don't really need I only need it for the websocket API, but it does work.

--- com.mechanitis.demo.sense.service/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.service/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.mechanitis.demo.sense.service {
     requires java.logging;
-    requires jetty.server;
-    requires jetty.servlet;
+    requires org.eclipse.jetty.server;
+    requires org.eclipse.jetty.servlet;
+    requires org.eclipse.jetty.websocket.javax.websocket.server;
     requires javax.websocket.api;
 
     exports com.mechanitis.demo.sense.service;
