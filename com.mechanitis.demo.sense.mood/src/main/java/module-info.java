447698b41bc5fabd4cbca8c7f2c51f9536610dd7
Trisha Gee <trisha.gee@gmail.com>
2021-03-04 11:52:41 +0100

    Adding an unused exports/opens to a module-info.java to see if the new inspections in IntelliJ IDEA are available yet.

--- com.mechanitis.demo.sense.mood/src/main/java/module-info.java
+++ com.mechanitis.demo.sense.mood/src/main/java/module-info.java
@@ -4,4 +4,7 @@ module com.mechanitis.demo.sense.mood {
     requires reactor.core;
     requires org.reactivestreams;
 
+    // these should be marked unused in some version of IntelliJ IDEA
+    exports com.mechanitis.demo.sense.mood;
+    opens com.mechanitis.demo.sense.mood;
 }
