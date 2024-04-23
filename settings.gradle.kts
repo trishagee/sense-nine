5e6a390ee405ad803b9170c7292aeb3b0044b067
Trisha <trisha.gee@gmail.com>
2024-04-23 12:22:51 +0200

    No Develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("com.gradle.enterprise") version "3.15.1"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.11.3"
-}
-
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
@@ -18,11 +13,3 @@ include("com.mechanitis.demo.sense.flow",
         "com.mechanitis.demo.sense.service.test",
         "com.mechanitis.demo.sense.twitter",
         "com.mechanitis.demo.sense.user")
-
-gradleEnterprise {
-    server = "https://18.212.219.143.nip.io"
-    allowUntrustedServer = true
-    buildScan {
-        publishAlways()
-    }
-}
